afc3aebac1c3006d065ecbefb6da533bb80dc6a1
Alok-Sharma <alok.sharma127@gmail.com>
2015-03-13 06:40:22 -0400

    Intent to move to main activity from login activity. Fixed app name.

--- mobile/src/main/java/aloksharma/ufl/edu/athome/loginActivity.java
+++ mobile/src/main/java/aloksharma/ufl/edu/athome/loginActivity.java
@@ -18,46 +18,50 @@ import com.parse.ParseUser;
  * Created by Soham Talukdar on 3/13/2015.
  * Login Page of the app.
  */
-public class loginActivity extends ActionBarActivity {
-protected EditText username;
-protected EditText password;
-protected Button signIn;
-protected Button signUp;
+public class LoginActivity extends ActionBarActivity {
+    protected EditText username;
+    protected EditText password;
+    protected Button signIn;
+    protected Button signUp;
+    Intent toMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        toMainActivity = new Intent(this, MainActivity.class);
+
         View someView = findViewById(R.id.login);
         View root = someView.getRootView();
         root.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_dark));
-    username=(EditText)findViewById(R.id.username);
-    password=(EditText)findViewById(R.id.password);
-    signIn=(Button)findViewById(R.id.signIn);
-    signUp=(Button)findViewById(R.id.signUp);
-    signIn.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
+        username=(EditText)findViewById(R.id.username);
+        password=(EditText)findViewById(R.id.password);
+        signIn=(Button)findViewById(R.id.signIn);
+        signUp=(Button)findViewById(R.id.signUp);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-            String userName=username.getText().toString().trim();
-            String passWord=password.getText().toString().trim();
-            ParseUser.logInInBackground(userName,passWord, new LogInCallback() {
-                public void done(ParseUser user, ParseException e) {
-                    if (user != null) {
-                        // Hooray! The user is logged in.
-                        Toast.makeText(loginActivity.this, "Successful sign-in", Toast.LENGTH_LONG).show();
-                    } else {
-                        // Signup failed. Look at the ParseException to see what happened.
-                        Toast.makeText(loginActivity.this, "Sign-in Failed :"+e.getMessage(), Toast.LENGTH_LONG).show();
+                String userName=username.getText().toString().trim();
+                String passWord=password.getText().toString().trim();
+                ParseUser.logInInBackground(userName,passWord, new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            // Hooray! The user is logged in.
+                            Toast.makeText(LoginActivity.this, "Successful sign-in", Toast.LENGTH_LONG).show();
+                            startActivity(toMainActivity);
+                        } else {
+                            // Signup failed. Look at the ParseException to see what happened.
+                            Toast.makeText(LoginActivity.this, "Sign-in Failed :"+e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
                     }
-                }
-            });
-        }
+                });
+            }
         });
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent takeRegPage= new Intent(loginActivity.this,Registration.class);
+                Intent takeRegPage= new Intent(LoginActivity.this,Registration.class);
                 startActivity(takeRegPage);
             }
         });
