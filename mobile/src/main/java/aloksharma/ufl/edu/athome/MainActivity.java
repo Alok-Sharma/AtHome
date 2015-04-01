37766a6ab7357734756ddfdbb40846112b682be4
Alok-Sharma <alok.sharma127@gmail.com>
2015-04-01 19:05:30 -0400

    Another small fix

--- mobile/src/main/java/aloksharma/ufl/edu/athome/MainActivity.java
+++ mobile/src/main/java/aloksharma/ufl/edu/athome/MainActivity.java
@@ -167,6 +167,7 @@ public class MainActivity extends Activity {
         ChangeWifiDialogFragment changeWifiDialogFragment = new ChangeWifiDialogFragment();
         if(wifiChecker.getWifiID(this) != null) {
             changeWifiDialogFragment.show(getFragmentManager(), "guitar");
+            requestToServer(ServerAccess.ServerAction.SET_HOME_STATUS);
         }else{
             Toast.makeText(this, "Not connected to a WiFi.", Toast.LENGTH_SHORT).show();
         }
